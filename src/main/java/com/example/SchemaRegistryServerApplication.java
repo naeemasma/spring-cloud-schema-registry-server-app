dac40fc7af15191da4cff3acb91abbc107578db7
naeemasma <59904237+naeemasma@users.noreply.github.com>
2020-07-28 08:32:37 -0400

    Initial commit

---
+++ src/main/java/com/example/SchemaRegistryServerApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.schema.server.EnableSchemaRegistryServer;
+
+@SpringBootApplication
+@EnableSchemaRegistryServer
+public class SchemaRegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaRegistryServerApplication.class, args);
+	}
+
+}
